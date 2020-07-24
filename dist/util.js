e45235f827772791c8161614b6778b0bc3e4a825
ln613 <ln613@hotmail.com>
2020-07-24 10:30:07 -0700

    extractUrl

--- dist/util.js
+++ dist/util.js
@@ -3,9 +3,15 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.admin = exports.api = exports.host = exports.isProd = exports.isDev = exports.port = exports.set = exports.get = exports.toLensPath = exports.diff = exports.isPrimitiveType = exports.toAbsDate = exports.toMonth = exports.toDate = exports.stringToPath = exports.isStringNumber = exports.escapeRegex = exports.replace = exports.toLowerDash = exports.toTitleCase = exports.split2 = exports.sortDesc = exports.sort = exports.addIndex = exports.getPropByProp = exports.getPropByName = exports.getNameById = exports.getPropById = exports.findByName = exports.findById = exports.findByProp = exports.isIn = exports.toSingleArray = exports.tap = void 0;
+exports.extractUrl = exports.extractHtml = exports.admin = exports.api = exports.host = exports.isProd = exports.isDev = exports.port = exports.diff = exports.isPrimitiveType = exports.set = exports.get = exports.toAbsDate = exports.toMonth = exports.toDate = exports.stringToPath = exports.replace = exports.isStringNumber = exports.escapeRegex = exports.toLowerDash = exports.toTitleCase = exports.addIndex = exports.split2 = exports.isIn = exports.toSingleArray = exports.sortDesc = exports.sort = exports.getPropByProp = exports.getPropByName = exports.getNameById = exports.getPropById = exports.findByName = exports.findById = exports.findByProp = exports.tap = exports.use = void 0;
 
 var _ramda = require("ramda");
+
+var _cheerio = _interopRequireDefault(require("cheerio"));
+
+var _axios = _interopRequireDefault(require("axios"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
@@ -13,9 +19,34 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var rePropName = /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!\2)[^\\]|\\.)*?)\2)\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g; // from lodash/fp
 
 var reEscapeChar = /\\(\\)?/g; // from lodash/fp
+
+var toLensPath = function toLensPath(p) {
+  return (0, _ramda.lensPath)((0, _ramda.is)(String, p) ? stringToPath(p) : p);
+};
+
+var findzz = function findzz(r, z) {
+  var zz = z.split(':');
+  var o = r.find(zz[0]);
+  if (o.length > 1 && zz.length > 1) return o.eq(zz[1]);
+  return o;
+}; // misc
+
+
+var use = function use() {
+  for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  return function (f) {
+    return f.apply(void 0, args);
+  };
+};
+
+exports.use = use;
 
 var tap = function tap(x) {
   var title = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
@@ -23,28 +54,16 @@ var tap = function tap(x) {
     return t;
   };
   var pred = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : true;
-  ((0, _ramda.is)(Function, pred) ? pred(x) : pred) && console.log(title ? "".concat(title, " - ") : '', f(x));
+
+  if ((0, _ramda.is)(Function, pred) ? pred(x) : pred) {
+    if (title) console.log("".concat(title, " - "), f(x));else console.log(f(x));
+  }
+
   return x;
 }; // array
 
 
 exports.tap = tap;
-
-var toSingleArray = function toSingleArray(arr) {
-  return (0, _ramda.is)(Array, arr) ? arr : [arr];
-};
-
-exports.toSingleArray = toSingleArray;
-
-var isIn = function isIn(arr) {
-  return function (val) {
-    return arr.some(function (item) {
-      return val === item;
-    });
-  };
-};
-
-exports.isIn = isIn;
 
 var findByProp = function findByProp(p) {
   return function (val) {
@@ -85,16 +104,6 @@ var getPropByProp = function getPropByProp(p1, p2, val) {
 };
 
 exports.getPropByProp = getPropByProp;
-
-var addIndex = function addIndex(p) {
-  return function (arr) {
-    return arr.map(function (x, i) {
-      return _objectSpread(_defineProperty({}, p || 'id', i + 1), (0, _ramda.dissoc)([p || 'id'], x));
-    });
-  };
-};
-
-exports.addIndex = addIndex;
 var sort = (0, _ramda.sort)(function (a, b) {
   return a - b;
 });
@@ -104,14 +113,40 @@ var sortDesc = (0, _ramda.sort)(function (a, b) {
 });
 exports.sortDesc = sortDesc;
 
+var toSingleArray = function toSingleArray(arr) {
+  return (0, _ramda.is)(Array, arr) ? arr : [arr];
+};
+
+exports.toSingleArray = toSingleArray;
+
+var isIn = function isIn(arr) {
+  return function (val) {
+    return arr.some(function (item) {
+      return val === item;
+    });
+  };
+};
+
+exports.isIn = isIn;
+
 var split2 = function split2(isCeil) {
   return function (arr) {
     return (0, _ramda.splitAt)((isCeil ? Math.ceil : Math.floor)(arr.length / 2), arr);
   };
+};
+
+exports.split2 = split2;
+
+var addIndex = function addIndex(p) {
+  return function (arr) {
+    return arr.map(function (x, i) {
+      return _objectSpread(_defineProperty({}, p || 'id', i + 1), (0, _ramda.dissoc)([p || 'id'], x));
+    });
+  };
 }; // string
 
 
-exports.split2 = split2;
+exports.addIndex = addIndex;
 
 var toTitleCase = function toTitleCase(s) {
   return s.replace(/\w\S*/g, function (t) {
@@ -127,14 +162,6 @@ var toLowerDash = function toLowerDash(s) {
 
 exports.toLowerDash = toLowerDash;
 
-var replace = function replace(s, params) {
-  return params && (0, _ramda.is)(Object, params) ? (0, _ramda.reduce)(function (p, c) {
-    return p.replace(new RegExp("{".concat(c, "}"), 'g'), params[c]);
-  }, s, Object.keys(params)) : s;
-};
-
-exports.replace = replace;
-
 var escapeRegex = function escapeRegex(s) {
   return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 };
@@ -146,6 +173,14 @@ var isStringNumber = function isStringNumber(s) {
 };
 
 exports.isStringNumber = isStringNumber;
+
+var replace = function replace(s, params) {
+  return params && (0, _ramda.is)(Object, params) ? (0, _ramda.reduce)(function (p, c) {
+    return p.replace(new RegExp("{".concat(c, "}"), 'g'), params[c]);
+  }, s, Object.keys(params)) : s;
+};
+
+exports.replace = replace;
 
 var stringToPath = function stringToPath(s) {
   // from lodash/fp
@@ -161,15 +196,17 @@ var stringToPath = function stringToPath(s) {
 exports.stringToPath = stringToPath;
 
 var toDate = function toDate(s) {
-  var d = new Date(s);
-  return "".concat(d.getMonth() + 1, "/").concat(d.getDate(), "/").concat(d.getFullYear());
+  return use(new Date(s))(function (d) {
+    return "".concat(d.getMonth() + 1, "/").concat(d.getDate(), "/").concat(d.getFullYear());
+  });
 };
 
 exports.toDate = toDate;
 
 var toMonth = function toMonth(s) {
-  var d = new Date(s);
-  return "".concat(d.getFullYear(), "/").concat(d.getMonth() + 1);
+  return use(new Date(s))(function (d) {
+    return "".concat(d.getFullYear(), "/").concat(d.getMonth() + 1);
+  });
 };
 
 exports.toMonth = toMonth;
@@ -180,6 +217,10 @@ var toAbsDate = function toAbsDate(d) {
 
 
 exports.toAbsDate = toAbsDate;
+var get = (0, _ramda.pipe)(toLensPath, _ramda.view);
+exports.get = get;
+var set = (0, _ramda.pipe)(toLensPath, _ramda.set);
+exports.set = set;
 var isPrimitiveType = (0, _ramda.anyPass)([(0, _ramda.is)(Number), (0, _ramda.is)(String), (0, _ramda.is)(Boolean)]);
 exports.isPrimitiveType = isPrimitiveType;
 
@@ -187,20 +228,10 @@ var diff = function diff(p) {
   return (0, _ramda.differenceWith)(function (a, b) {
     return isPrimitiveType(a) ? a === b : a[p || 'id'] === b[p || 'id'];
   });
-};
+}; // env
+
 
 exports.diff = diff;
-
-var toLensPath = function toLensPath(p) {
-  return (0, _ramda.lensPath)((0, _ramda.is)(String, p) ? stringToPath(p) : p);
-};
-
-exports.toLensPath = toLensPath;
-var get = (0, _ramda.pipe)(toLensPath, _ramda.view);
-exports.get = get;
-var set = (0, _ramda.pipe)(toLensPath, _ramda.set); // env
-
-exports.set = set;
 var port = process.env.PORT || 3000;
 exports.port = port;
 
@@ -223,8 +254,11 @@ var admin = host + 'admin/'; // html
 
 exports.admin = admin;
 
-var extract = function extract(html, opt) {
+var extractHtml = function extractHtml(html, opt) {
   var o = {};
+
+  var r = _cheerio["default"].load(html);
+
   opt.forEach(function (x) {
     o[x[1]] = r(x[0]).map(function (i, y) {
       // x[0] - root element(s), x[1] - output root object name
@@ -232,17 +266,17 @@ var extract = function extract(html, opt) {
       var r1 = r(r(x[0])[i]);
       x[2].forEach(function (z) {
         // z[0] - child element(s), z[1] - output property name, 
-        var z0 = z[0] ? R.is(String, z[0]) ? (0, _ramda.find)(r1, z[0]) : z[0](r1) : r1; // child element(s) can be a cheerio func expecting root element
+        var z0 = z[0] ? (0, _ramda.is)(String, z[0]) ? findzz(r1, z[0]) : z[0](r1) : r1; // child element(s) can be a cheerio func expecting root element
 
         var a1 = z0.length > 1; // is child element(s) an array?
 
-        var a2 = R.is(Array, z[2]); // is attr(s) an array?
+        var a2 = (0, _ramda.is)(Array, z[2]); // is attr(s) an array?
 
         o1[z[1]] = a2 ? a1 ? z0.map(function (j, u) {
-          return R.fromPairs(z[2].map(function (w) {
+          return (0, _ramda.fromPairs)(z[2].map(function (w) {
             return [w[0], r(u).attr(w[1])];
           }));
-        }).toArray() : R.fromPairs(z[2].map(function (w) {
+        }).toArray() : (0, _ramda.fromPairs)(z[2].map(function (w) {
           return [w[0], z0.attr(w[1])];
         })) : a1 ? z0.map(function (j, u) {
           return z[2] ? r(u).attr(z[2]) : r(u).text();
@@ -253,3 +287,13 @@ var extract = function extract(html, opt) {
   });
   return o;
 };
+
+exports.extractHtml = extractHtml;
+
+var extractUrl = function extractUrl(url, opt) {
+  return _axios["default"].get(url).then(function (r) {
+    return extractHtml(r.data, opt);
+  });
+};
+
+exports.extractUrl = extractUrl;
--- src/util.js
+++ src/util.js
@@ -1,40 +1,56 @@
-import { sort as _sort, find, is, isNil, pipe, reduce, prop, differenceWith, anyPass, splitAt, dissoc, lensPath, view, set as _set, over } from 'ramda';
+import { sort as _sort, find, is, isNil, pipe, reduce, prop, differenceWith, anyPass, splitAt, dissoc, lensPath, view, set as _set, over, fromPairs } from 'ramda'
+//import $ from 'jquery'
+import cheerio from 'cheerio'
+import axios from 'axios'
 
-const rePropName = /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!\2)[^\\]|\\.)*?)\2)\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g; // from lodash/fp
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
+const rePropName = /[^.[\]]+|\[(?:(-?\d+(?:\.\d+)?)|(["'])((?:(?!\2)[^\\]|\\.)*?)\2)\]|(?=(?:\.|\[\])(?:\.|\[\]|$))/g // from lodash/fp
 const reEscapeChar = /\\(\\)?/g; // from lodash/fp
+const toLensPath = p => lensPath(is(String, p) ? stringToPath(p) : p)
+const findzz = (r, z) => {
+  const zz = z.split(':');
+  const o = r.find(zz[0]);
+  if (o.length > 1 && zz.length > 1)
+    return o.eq(zz[1]);
+  return o;
+}
 
+// misc
+
+export const use = (...args) => f => f(...args)
 export const tap = (x, title = '', f = t => t, pred = true) => {
-  (is(Function, pred) ? pred(x) : pred) && console.log(title ? `${title} - ` : '', f(x))
+  if (is(Function, pred) ? pred(x) : pred) {
+    if (title) console.log(`${title} - `, f(x))
+    else console.log(f(x))
+  }
   return x
 }
 
 // array
 
-export const toSingleArray = arr => is(Array, arr) ? arr : [arr];
-
-export const isIn = arr => val => arr.some(item => val === item);
-
 export const findByProp = p => val => arr => find(x => x[p] == val, arr || []);
 export const findById = findByProp('id');
 export const findByName = findByProp('name');
-
 export const getPropById = p => id => pipe(findById(id), prop(p));
 export const getNameById = getPropById('name')
 export const getPropByName = p => name => pipe(findByName(name), prop(p));
 export const getPropByProp = (p1, p2, val) => pipe(findByProp(p2)(val), prop(p1));
 
-export const addIndex = p => arr => arr.map((x, i) => ({ [p || 'id']: i + 1, ...dissoc([p || 'id'], x) }));
-
 export const sort = _sort((a, b) => a - b);
 export const sortDesc = _sort((a, b) => b - a);
 
+export const toSingleArray = arr => is(Array, arr) ? arr : [arr];
+export const isIn = arr => val => arr.some(item => val === item);
 export const split2 = isCeil => arr => splitAt((isCeil ? Math.ceil : Math.floor)(arr.length / 2), arr);
+export const addIndex = p => arr => arr.map((x, i) => ({ [p || 'id']: i + 1, ...dissoc([p || 'id'], x) }));
 
 // string
 
 export const toTitleCase = s => s.replace(/\w\S*/g, t => t.charAt(0).toUpperCase() + t.substr(1).toLowerCase());
-
 export const toLowerDash = s => s.toLowerCase().replace(/ /g, '-');
+export const escapeRegex = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+export const isStringNumber = s => !isNaN(+s);
 
 export const replace = (s, params) => params && is(Object, params)
   ? reduce(
@@ -43,11 +59,6 @@ export const replace = (s, params) => params && is(Object, params)
     Object.keys(params)
   )
   : s;
-
-export const escapeRegex = s =>
-  s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-
-export const isStringNumber = s => !isNaN(+s);
 
 export const stringToPath = s => {  // from lodash/fp
   const result = [];
@@ -60,54 +71,41 @@ export const stringToPath = s => {  // from lodash/fp
 
 // date
 
-export const toDate = s => {
-  const d = new Date(s);
-  return `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
-}
-
-export const toMonth = s => {
-  const d = new Date(s);
-  return `${d.getFullYear()}/${d.getMonth() + 1}`;
-}
-
+export const toDate = s => use(new Date(s))(d => `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`)
+export const toMonth = s => use(new Date(s))(d => `${d.getFullYear()}/${d.getMonth() + 1}`)
 export const toAbsDate = d => new Date(d).toISOString().slice(0, 10);
 
 // object
 
-export const isPrimitiveType = anyPass([is(Number), is(String), is(Boolean)]);
-
-export const diff = p => differenceWith((a, b) => isPrimitiveType(a) ? a === b : a[p || 'id'] === b[p || 'id']);
-
-export const toLensPath = p => lensPath(is(String, p) ? stringToPath(p) : p);
-
 export const get = pipe(toLensPath, view);
-
 export const set = pipe(toLensPath, _set);
+export const isPrimitiveType = anyPass([is(Number), is(String), is(Boolean)]);
+export const diff = p => differenceWith((a, b) => isPrimitiveType(a) ? a === b : a[p || 'id'] === b[p || 'id']);
 
 // env
 
 export const port = process.env.PORT || 3000;
 export const isDev = () => process.env.NODE_ENV && isIn(['development', 'dev'])(process.env.NODE_ENV.toLowerCase());
 export const isProd = () => process.env.NODE_ENV || isIn(['production', 'prod'])(process.env.NODE_ENV.toLowerCase());
-
 export const host = isDev() ? `http://localhost:${port}/` : '/';
 export const api = host + 'api/';
 export const admin = host + 'admin/';
 
 // html
 
-const extract = (html, opt) => {
+export const extractHtml = (html, opt) => {
   const o = {};
+  const r = cheerio.load(html);
   opt.forEach(x => {
     o[x[1]] = r(x[0]).map((i, y) => { // x[0] - root element(s), x[1] - output root object name
       const o1 = {};
       const r1 = r(r(x[0])[i]);
       x[2].forEach(z => { // z[0] - child element(s), z[1] - output property name, 
-        const z0 = z[0] ? (R.is(String, z[0]) ? find(r1, z[0]) : z[0](r1)) : r1; // child element(s) can be a cheerio func expecting root element
+        const z0 = z[0] ? (is(String, z[0]) ? findzz(r1, z[0]) : z[0](r1)) : r1; // child element(s) can be a cheerio func expecting root element
         const a1 = z0.length > 1; // is child element(s) an array?
-        const a2 = R.is(Array, z[2]); // is attr(s) an array?
+        const a2 = is(Array, z[2]); // is attr(s) an array?
         o1[z[1]] = a2 ?
-          (a1 ? z0.map((j, u) => R.fromPairs(z[2].map(w => [w[0], r(u).attr(w[1])]))).toArray() : R.fromPairs(z[2].map(w => [w[0], z0.attr(w[1])]))) :
+          (a1 ? z0.map((j, u) => fromPairs(z[2].map(w => [w[0], r(u).attr(w[1])]))).toArray() : fromPairs(z[2].map(w => [w[0], z0.attr(w[1])]))) :
           (a1 ? z0.map((j, u) => z[2] ? r(u).attr(z[2]) : r(u).text()).toArray() : (z[2] ? z0.attr(z[2]) : z0.text()));
       });
       return o1;
@@ -115,3 +113,5 @@ const extract = (html, opt) => {
   });
   return o;
 }
+
+export const extractUrl = (url, opt) => axios.get(url).then(r => extractHtml(r.data, opt))
