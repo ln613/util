29060def2326b874d842a0ab6a36033fe651581b
ln613 <ln613@hotmail.com>
2020-07-29 12:05:03 -0700

    bundle

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+entry: './src/index.js',
+output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+},
+module: {
+    rules: [
+        {
+            test: /\.js$/, //using regex to tell babel exactly what files to transcompile
+            exclude: /node_modules/, // files to be ignored
+            use: {
+                loader: 'babel-loader' // specify the loader
+            } 
+        }
+    ]
+}
+}
