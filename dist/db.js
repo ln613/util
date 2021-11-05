01375a7c960007fdd3e9c70037caf3a9c0e93b9e
ln613 <ln613@hotmail.com>
2021-11-05 00:16:06 -0700

    db

--- dist/db.js
+++ dist/db.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
+
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
 Object.defineProperty(exports, "__esModule", {
@@ -13,7 +15,9 @@ var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/de
 
 var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
 
-var _mongodb = require("mongodb");
+var _mongodb = _interopRequireWildcard(require("mongodb"));
+
+var _mongodbClientEncryption = require("mongodb-client-encryption");
 
 var _ramda = require("ramda");
 
--- src/db.js
+++ src/db.js
@@ -1,3 +1,5 @@
+import mongodb from 'mongodb';
+import { ClientEncryption } from 'mongodb-client-encryption';
 import { MongoClient } from 'mongodb';
 import { fromPairs, is, merge, cond } from 'ramda';
 import { C, N, T, P, noneEmptyArray, noneEmptyObject, tap } from './util';
